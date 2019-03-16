53239633b2b98d8df4b57191011f2de7475c8be1
Alejandro <soniccrest34@gmail.com>
2019-03-16 18:29:57 -0500

    Game of Life

---
+++ PlayingWithProcessing/src/GameOfLife/GoL.java
@@ -0,0 +1,38 @@
+package GameOfLife;
+
+import processing.core.PApplet;
+
+public class GoL extends PApplet {
+	
+	public class Logic {
+		
+
+		public Logic()
+		{
+			init();
+		}
+		
+		public void init()
+		{
+			
+		}
+		
+	}
+	
+	public void settings() {
+		
+	}
+	
+	public void draw() {
+		
+		
+	}
+	
+
+	public static void main(String[] args) {
+		
+		
+	
+	}
+
+}
