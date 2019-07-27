bf1e58f6d73543ac3ed0d11685050cadfb94833b
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2019-07-26 20:03:55 -0500

    Arreglar renderizado de circulos

--- PlayingWithProcessing/src/triqui/Triqui.java
+++ PlayingWithProcessing/src/triqui/Triqui.java
@@ -14,32 +14,45 @@ public class Triqui extends PApplet {
 	}
 	
 	Logic Triqui = new Logic();
+	public int width = 400, heigth = 400;
 
 	public void settings () {
-		size(400,400);
+		size(width, heigth);
 	}
 	
 	public void draw () {
 		
 		background(100);
 		
-		rect(0, 0, 400/3, 400/3);
+		rect(0, 0, width/3, heigth/3);
 		
-		rect(400/3, 0, 400/3, 400/3);
+		rect(400/3, 0, width/3, heigth/3);
 		
-		rect((400/3)*2, 0, 400/3, 400/3);
+		rect((400/3)*2, 0, width/3, heigth/3);
 		
-		rect(0, 400/3, 400/3, 400/3);
+		rect(0, 400/3, width/3, heigth/3);
 		
-		rect(400/3, 400/3, 400/3, 400/3);
+		rect(400/3, 400/3, width/3, heigth/3);
 		
-		rect((400/3)*2, 400/3, 400/3, 400/3);
+		rect((400/3)*2, 400/3, width/3, heigth/3);
 		
-		rect(0, (400/3)*2, 400/3, 400/3);
+		rect(0, (400/3)*2, width/3, heigth/3);
 		
-		rect(400/3, (400/3)*2, 400/3, 400/3);
+		rect(400/3, (400/3)*2, width/3, heigth/3);
 		
-		rect((400/3)*2, (400/3)*2, 400/3, 400/3);
+		rect((400/3)*2, (400/3)*2, width/3, heigth/3);
+		
+		for(int i = 0; i < 3; i++) 
+			for(int j = 0; j < 3; j++) {
+				if(Triqui.board[i][j] == 1) {
+					line(j*(400/3), i*(400/3), (j+1)*(400/3), (i+1)*(400/3));
+					line((j+1)*(400/3), i*(400/3), j*(400/3), (i+1)*(400/3));
+				} else
+					if(Triqui.board[i][j] == 0){
+						ellipseMode(CENTER);
+						ellipse(((j+1)*(400/3))/2, ((i+1)*(400/3))/2, width/3, heigth/3);
+					}
+			}
 	}
 	
 	public static void main(String[] args) {
