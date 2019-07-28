ec86eff5b5d695fbd583f3a53b9fcd6eb8090557
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2019-07-28 18:29:18 -0500

    Generación de tablero mejorada

--- PlayingWithProcessing/src/triqui/Triqui.java
+++ PlayingWithProcessing/src/triqui/Triqui.java
@@ -25,23 +25,9 @@ public class Triqui extends PApplet {
 		
 		background(100);
 		
-		rect(0, 0, width/3, heigth/3);
-		
-		rect(400/3, 0, width/3, heigth/3);
-		
-		rect((400/3)*2, 0, width/3, heigth/3);
-		
-		rect(0, 400/3, width/3, heigth/3);
-		
-		rect(400/3, 400/3, width/3, heigth/3);
-		
-		rect((400/3)*2, 400/3, width/3, heigth/3);
-		
-		rect(0, (400/3)*2, width/3, heigth/3);
-		
-		rect(400/3, (400/3)*2, width/3, heigth/3);
-		
-		rect((400/3)*2, (400/3)*2, width/3, heigth/3);
+		for(int i = 0; i < 3; i++) 
+			for(int j = 0; j < 3; j++)
+				rect(j*(width/3), i*(heigth/3), width/3, heigth/3);
 		
 		for(int i = 0; i < 3; i++) 
 			for(int j = 0; j < 3; j++) {
@@ -49,7 +35,7 @@ public class Triqui extends PApplet {
 					line(j*(width/3), i*(heigth/3), (j+1)*(width/3), (i+1)*(heigth/3));
 					line((j+1)*(width/3), i*(heigth/3), j*(width/3), (i+1)*(heigth/3));
 				} else
-					if(Triqui.board[i][j] == 0){
+					if(Triqui.board[i][j] == 2){
 						ellipseMode(CORNERS);
 						ellipse(j*(width/3), i*(heigth/3), (j+1)*(width/3), (i+1)*(heigth/3));
 					}
